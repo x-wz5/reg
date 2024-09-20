a9c2d780664be2c32c0f29c09cde5185dc04837e
x-wz5 <121063923+x-wz5@users.noreply.github.com>
2024-09-20 23:23:34 +0300

    Update wefwwffwef.lua

--- wefwwffwef.lua
+++ wefwwffwef.lua
@@ -1,7 +1,44 @@
 local result = ""
 local s = game.Players.LocalPlayer.PlayerScripts.LocalScript
 local opcodes = {
-    [22] = "JMP" -- Make sure to add more opcodes as necessary
+    [0] = "MOVE",
+    [1] = "LOADK",
+    [2] = "LOADBOOL",
+    [3] = "LOADNIL",
+    [4] = "GETUPVAL",
+    [5] = "GETGLOBAL",
+    [6] = "GETTABLE",
+    [7] = "SETGLOBAL",
+    [8] = "SETUPVAL",
+    [9] = "SETTABLE",
+    [10] = "NEWTABLE",
+    [11] = "SELF",
+    [12] = "ADD",
+    [13] = "SUB",
+    [14] = "MUL",
+    [15] = "DIV",
+    [16] = "MOD",
+    [17] = "POW",
+    [18] = "UNM",
+    [19] = "NOT",
+    [20] = "LEN",
+    [21] = "CONCAT",
+    [22] = "JMP",
+    [23] = "EQ",
+    [24] = "LT",
+    [25] = "LE",
+    [26] = "TEST",
+    [27] = "TESTSET",
+    [28] = "CALL",
+    [29] = "TAILCALL",
+    [30] = "RETURN",
+    [31] = "FORLOOP",
+    [32] = "FORPREP",
+    [33] = "TFORLOOP",
+    [34] = "SETLIST",
+    [35] = "CLOSE",
+    [36] = "CLOSURE",
+    [37] = "VARARG"
 }
 
 -- Corrected parseBytecode function
@@ -10,13 +47,8 @@ function parseBytecode(bytecode)
     local i = 1
     while i <= #bytecode do
         local byte = string.byte(bytecode, i)
-        local opc = opcodes[byte]
-
-        -- Ensure that the opcode is defined, otherwise skip this instruction
-        if opc then
-            table.insert(instructions, {opcode = opc, byte = byte, line = i})
-        end
-
+        local opc = opcodes[byte] -- Lookup the opcode based on byte value
+        table.insert(instructions, {opcode = opc, byte = byte, line = i})
         i = i + 1 -- Increment to avoid infinite loop
     end
     return instructions
@@ -40,6 +72,8 @@ local function processBytecode(bytecode)
                 targetLine = instr.line + negativeOffset + 1
                 print("JMP at line " .. instr.line .. " jumps backward to line " .. targetLine)
             end
+        else
+            print("Opcode " .. instr.opcode .. " found at line " .. instr.line)
         end
     end
 end
